c6ba0507277ed5b89da7ef7e6373c693928e37f7
Marie <mariela.gutierrez2906@gmail.com>
2023-06-05 22:21:13 +0300

    Game Attrb Component.

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,18 +1,23 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
+import ExpandableText from "../components/ExpandableText";
+import GameAttrb from "../components/GameAttrb";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
-  const {data:game, isLoading, error} = useGame(slug!)
+  const { data: game, isLoading, error } = useGame(slug!);
 
-  if(isLoading) return <Spinner/>
+  if (isLoading) return <Spinner />;
 
-  if(error || !game) throw error
-  return <>
-    <Heading>{ game.name}</Heading>
-    <Text>{game.description_raw}</Text>
-  </>
+  if (error || !game) throw error;
+  return (
+    <>
+      <Heading>{game.name}</Heading>
+      <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttrb game={game}></GameAttrb>
+    </>
+  );
 };
 
 export default GameDetailPage;
